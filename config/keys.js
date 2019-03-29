dc84fbf0deea61da4987ca29b223ebc26d4f7367
rteina <rteina@galigeo.com>
2019-03-29 20:05:29 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,9 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+'use strict';
+
+//dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+
+let dbPassword = 'mongodb+srv://rtaggo:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@rtaggocluster-f2nqm.mongodb.net/test?retryWrites=true';
 
 module.exports = {
-    mongoURI: dbPassword
+  mongoURI: dbPassword
 };
